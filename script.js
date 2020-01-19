64ad1de5b2995fd84d48625f2d0ca5c0f94cdd38
Özlem Karaduman <ozlemk.duman@gmail.com>
2020-01-19 14:56:20 +0300

    algoritma eklendi

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 // algoritma çıkar
 // kullanıcı arayüzü nasıl çalışcak bunu da adım adım belirt
 
-var info = document.querySelector(".info");
-var answer = document.querySelector(".answer");
-var inputla = document.querySelector("#inputid");
-var submit = document.querySelector("#submitid");
+// ekranda oyunu başlatma butonu olsun.
+// butona tıklayınca oyun ekranı gelsin
+// oyun ekranında oyun sorusu, sayı tahmin inputu, ve sonuç bölümlerinin olduğu 3 alan olsun.
+// kullanıcının 5 hakkı olsun 
+// bilirse ekranda kazandınız yazsın ve oyun başlat ekranına dönsün
+// bilemezse 1 hakkı gitsin ve sonuç bölümünde bilemediniz yazsın. kalan hakkı göstersin
+// 5 hakkı biterse sonuç bölümünde kazanamadınız yazsın ve ekranda ana ekrana dön butonu belirsin.
 
-var hak = 5;
-var durum = false;
+let info = document.querySelector(".info");
+let answer = document.querySelector(".answer");
+let inputla = document.querySelector("#inputid");
+let submit = document.querySelector("#submitid");
+
+let hak = 5;
+let durum = false;
 info.innerHTML = "merhaba Lütfen 0 ile 100 arasında sayı giriniz. 5 hakkınız var";
 
-var rastgele = Math.floor(Math.random() * 101);
+let rastgele = Math.floor(Math.random() * 101);
 
 function baslat() {
     info.style.display = "block"
@@ -20,7 +28,7 @@ function baslat() {
 }
 
 function tahminlendi() {
-    var tahmin = inputla.value;
+    let tahmin = inputla.value;
     console.log({ tahmin, rastgele });
     if (tahmin == rastgele && durum == true && hak > 0) {
         answer.innerHTML = "tebrikler bildiniz!";
