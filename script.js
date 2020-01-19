83689a6c7d6aee3fabb12b751c931b7db07620f0
Hakan Karaduman <karadumanhakan1@gmail.com>
2020-01-19 14:42:57 +0300

    notlar eklendi

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-var info=document.querySelector(".info");
-var answer=document.querySelector(".answer");
-var inputla=document.querySelector("#inputid");
-var submit=document.querySelector("#submitid");
+// var kullanma, let ve const kullan
+// algoritma çıkar
+// kullanıcı arayüzü nasıl çalışcak bunu da adım adım belirt
 
+var info = document.querySelector(".info");
+var answer = document.querySelector(".answer");
+var inputla = document.querySelector("#inputid");
+var submit = document.querySelector("#submitid");
 
+var hak = 5;
+var durum = false;
+info.innerHTML = "merhaba Lütfen 0 ile 100 arasında sayı giriniz. 5 hakkınız var";
 
+var rastgele = Math.floor(Math.random() * 101);
 
-var hak=5;
-var durum=false;
-info.innerHTML="merhaba Lütfen 0 ile 100 arasında sayı giriniz. 5 hakınız var";
-
-
-var rastgele=Math.floor(Math.random()*101);
-
-
-function baslat(){
-    console.log("blabla");
+function baslat() {
+    info.style.display = "block"
     console.log(`rastgele sayı= ${rastgele}`);
-    durum=true;
+    durum = true;
 }
 
-
-function tahminlendi(){
-    var tahmin=inputla.value;
-    console.log({tahmin,rastgele});
-
-    if(tahmin==rastgele && durum==true && hak>0){
-        answer.innerHTML="tebrikler bildiniz!";
-        durum=false;
-    }else {
-        answer.innerHTML="malesef kazanamadınız...";
+function tahminlendi() {
+    var tahmin = inputla.value;
+    console.log({ tahmin, rastgele });
+    if (tahmin == rastgele && durum == true && hak > 0) {
+        answer.innerHTML = "tebrikler bildiniz!";
+        durum = false;
+    } else {
+        answer.innerHTML = "malesef bilemedin...";
         hak--;
         console.log(hak);
-        if(hak<1){
-            durum=false;
-            info.innerHTML="Hakkınız bitmiştir. Yeniden oynamak için lütfen başlat butonuna basın";
-
+        if (hak < 1) {
+            durum = false;
+            info.innerHTML = "Hakkınız bitmiştir. Yeniden oynamak için lütfen başlat butonuna basın";
         }
     }
-
 }
