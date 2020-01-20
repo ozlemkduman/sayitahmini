4c5fff73fccfdca730220c15bf7166ffb35355f6
Özlem Karaduman <ozlemk.duman@gmail.com>
2020-01-20 16:44:13 +0300

    bitti

--- script.js
+++ script.js
@@ -5,80 +5,88 @@
 // ekranda oyunu başlatma butonu olsun.  +
 // butona tıklayınca oyun ekranı gelsin  +
 // oyun ekranında oyun sorusu, sayı tahmin inputu, tahmin yapma butonu ve sonuç bölümlerinin olduğu 3 alan olsun.  +
-// kullanıcının 5 hakkı olsun  
-// sadece sayı girebilsin - type "number"
-// bilirse ekranda kazandınız yazsın ve oyun başlat ekranına dönsün
-// bilemezse 1 hakkı gitsin ve sonuç bölümünde bilemediniz yazsın. kalan hakkı göstersin
-// 5 hakkı biterse sonuç bölümünde kazanamadınız yazsın ve ekranda ana ekrana dön butonu belirsin.
+// kullanıcının 5 hakkı olsun  +
+// sadece sayı girebilsin - type "number"  +
+// bilirse ekranda kazandınız yazsın ve oyun başlat ekranına dönsün  +
+// bilemezse 1 hakkı gitsin ve sonuç bölümünde bilemediniz yazsın. kalan hakkı göstersin +
+// 5 hakkı biterse sonuç bölümünde kazanamadınız yazsın ve ekranda ana ekrana dön butonu belirsin.  +
 
-// input tahminButonu sonuç başlatmaButonu oyunSorusu rastgeleSayi hak
-// sadece başlatmaButonu görünür
-// başlatmaButonu'na tıklanınca oyun ekranı görünür
-// oyun başladığında
+// input tahminButonu sonuç baslatmaButonu oyunSorusu rastgeleSayi hak
+
+//oyun başladığında 
+
+    //baslatmaButonu
+        //rastgeleSayı belirlenir
+        //hak tanımlanır
+        //ekranda oyunSorusu görünür
+        // input
+            //yazı yazabilir
+                //type=number ile engelle
+            //sayı yazabilir
+                //negatif yazabilir
+                    //<21>
+                    //inputu boşalt
+                //pozitif yazabilir
+                    //24
+        
+    //hak
+            //hak yeterliyse
+                //tahminButonu aktif
+            //hak yetersizse
+                //tahminButonu pasif
+                //(50)        
+    //tahminButonu
+        //rastgele sayı ile tahmin karşılaştır
+            //doğruysa
+                //<48>
+            //yanlışsa
+                // hak -1 azalsın, ekranda malesef bilemediniz tekrar deneyin  {hak} kaldı yazsın. 
+                // <32>
+
+//oyun bittiğinde
+    //kazandıysa
+        //ekranda tebrikler kazandınız yazsın ve baslatma butonu gözüksün
+    //kaybettiyse
+        //ekranda malesef kazanamadınız yazsın ve baslatma butonu görünsün
 
 
-  //hak kontrol
-        //hak yeterliyse
-            // rastgeleSayi 0-100 arası bir sayıya eşitlenir
-            // oyunSorusu "0-100 arası bir sayı seçin, {hak} hakkınız var" yazısını gösterir
-            // input'a yazı yazabilir
-                // type=number yaparak engelle
-            // input'a sayı yazabilir
-                // negatif yazabilir
-                    // 0 ile 100 arasında yazın uyarısı gelicek, sonuç ekranında
-                    // inputu boşalt
-            // 0 yazabilir
-                // <38>
-            // pozitif yazabilir
-                // <38>
+        let rastgeleSayi;
+        let hak;
+        let input=document.getElementById("inputid");
+        let inputValue;
+function baslatmaButonu(){
+    document.getElementsByClassName("answer")[0].innerHTML="";
+    rastgeleSayi=Math.floor(Math.random()*10);
+    input.value="";
+    hak=5;
+    document.getElementsByClassName("container")[0].style.display="flex";
+    document.getElementById("basla").style.display="none";
+    console.log(rastgeleSayi);
+    document.getElementsByClassName("oyunSorusu")[0].innerHTML="1 ile 10 arası bir sayı tahmin ediniz..";
+    input.addEventListener("change", function(){
+        inputValue=input.value;
+    })
 
-        //hak yetersizse
-            //<oyun bittiğinde> çalışır
-
-
-  // tahminButonuna basılırsa
-        // input boşsa
-            //<28>
-        // input doluysa
-            //rastgele sayı ile input karşılaştır
-                //tahmin doğru ise sonuç ekraında  kazandınız yazsın ve <bittiğinde> çalışsın
-                //tahmin yanlış ise hak -1 azalsın ve sonuç ekraında bilemediniz {hak} kaldı yazsın
      
-     
-  
-// oyun bittiğinde
-  //ekranda ana ekrana dön butonu belirsin.
-
-let info = document.querySelector(".info");
-let answer = document.querySelector(".answer");
-let inputla = document.querySelector("#inputid");
-let submit = document.querySelector("#submitid");
-
-let hak = 5;
-let durum = false;
-info.innerHTML = "merhaba Lütfen 0 ile 100 arasında sayı giriniz. 5 hakkınız var";
-
-let rastgele = Math.floor(Math.random() * 101);
-
-function baslat() {
-    info.style.display = "block"
-    console.log(`rastgele sayı= ${rastgele}`);
-    durum = true;
 }
+let submitid = document.getElementById("submitid");
 
-function tahminlendi() {
-    let tahmin = inputla.value;
-    console.log({ tahmin, rastgele });
-    if (tahmin == rastgele && durum == true && hak > 0) {
-        answer.innerHTML = "tebrikler bildiniz!";
-        durum = false;
-    } else {
-        answer.innerHTML = "malesef bilemedin...";
+submitid.addEventListener("click", function tahminButonu(){
+    
+    if(rastgeleSayi==inputValue){
+        console.log("doğru");
+        document.getElementsByClassName("answer")[0].innerHTML="tebrikler Kazandınız";
+        setTimeout(function(){ document.getElementsByClassName("container")[0].style.display="none";
+        document.getElementById("basla").style.display="Block"; }, 2000);
+    }else if(hak>1){
         hak--;
-        console.log(hak);
-        if (hak < 1) {
-            durum = false;
-            info.innerHTML = "Hakkınız bitmiştir. Yeniden oynamak için lütfen başlat butonuna basın";
-        }
+        console.log("yanlış");
+        document.getElementsByClassName("answer")[0].innerHTML=`malesef bilemediniz ${hak} hakkınız kaldı`;
+    }else if (hak==1){
+        document.getElementsByClassName("answer")[0].innerHTML="Bilemediniz.Hakkınız Kalmadı. Oyunu Tekrar başlatın";
+        setTimeout(function(){ document.getElementsByClassName("container")[0].style.display="none";
+        document.getElementById("basla").style.display="Block"; }, 2000);
     }
-}
+});
+
+
